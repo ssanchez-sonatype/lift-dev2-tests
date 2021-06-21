eb554e6c2107116849a396ddc3902b329eec290f
Amelia Keibler <akeibler@sonatype.com>
2021-06-21 10:38:34 -0400

    Initial custom tool

---
+++ .lift/tool.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+dir=$1
+commit=$2
+cmd=$3 
+
+function version() {
+    echo 1
+}
+
+function applicable() {
+    echo "true"
+}
+
+function run() {
+    echo "[{ \"type\": \"Hello World\", \
+            \"message\": \"We are analyzing commit $commit\", \
+            \"file\": \"N/A\", \
+            \"line\": 0 \
+          }]"
+}
+
+if [[ "$cmd" = "run" ]] ; then 
+    run 
+fi 
+if [[ "$cmd" = "applicable" ]] ; then 
+    applicable 
+fi 
+if [[ "$cmd" = "version" ]] ; then 
+    version 
+fi
